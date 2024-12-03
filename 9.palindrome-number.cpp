58f77a5ac7435a57ddd20e8ab8302e1da1eb82c9
Kexin-Wei <vacationwkx@gmail.com>
2024-12-03 23:15:31 +0800

    # Easy 88

--- 9.palindrome-number.cpp
+++ 9.palindrome-number.cpp
@@ -2,13 +2,59 @@
  * @lc app=leetcode id=9 lang=cpp
  *
  * [9] Palindrome Number
+ *
+ * https://leetcode.com/problems/palindrome-number/description/
+ *
+ * algorithms
+ * Easy (58.01%)
+ * Likes:    13244
+ * Dislikes: 2779
+ * Total Accepted:    5.5M
+ * Total Submissions: 9.6M
+ * Testcase Example:  '121'
+ *
+ * Given an integer x, return true if x is a palindrome, and false
+ * otherwise.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: x = 121
+ * Output: true
+ * Explanation: 121 reads as 121 from left to right and from right to left.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: x = -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it
+ * becomes 121-. Therefore it is not a palindrome.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: x = 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a
+ * palindrome.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * -2^31 <= x <= 2^31 - 1
+ *
+ *
+ *
+ * Follow up: Could you solve it without converting the integer to a string?
  */
 
 // @lc code=start
-#include <iostream>
-#include <vector>
-
-using namespace std;
 class Solution {
  public:
   bool isPalindrome(int x) {
@@ -24,12 +70,3 @@ class Solution {
   }
 };
 // @lc code=end
-
-int main() {
-  Solution s;
-  cout << s.isPalindrome(121) << endl;
-  cout << s.isPalindrome(-121) << endl;
-  cout << s.isPalindrome(10) << endl;
-
-  return 0;
-}
