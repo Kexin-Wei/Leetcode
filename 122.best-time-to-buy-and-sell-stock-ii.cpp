62111a3c559796eed6fa1f782a35871477ae5cee
Kexin <vacationwkx@gmail.com>
2025-07-08 22:50:24 +0800

    easy 122

--- 122.best-time-to-buy-and-sell-stock-ii.cpp
+++ 122.best-time-to-buy-and-sell-stock-ii.cpp
@@ -77,19 +77,13 @@ using namespace std;
 class Solution {
  public:
   int maxProfit(vector<int>& prices) {
-    int max_profit = 0;
     int profit = 0;
     for (size_t i = 1; i < prices.size(); i++) {
       if (prices[i] > prices[i - 1]) {
         profit += prices[i] - prices[i - 1];
-      } else if (profit > 0) {
-        if (profit > max_profit) {
-          max_profit = profit;
-        }
-        profit = 0;
       }
     }
-    return max_profit;
+    return profit;
   }
 };
 // @lc code=end
